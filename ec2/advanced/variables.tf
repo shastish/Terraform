755d37560e6300ffe79f95cfb8982b1c50c81c82
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-11-29 12:23:37 +0530

    Create variables.tf

---
+++ ec2/advanced/variables.tf
@@ -0,0 +1,15 @@
+variable "publicsubnetid" {
+    default = "subnet-0f44ac1bacfdcc23b"
+}
+variable "privatesubnetid" {
+    default = "subnet-0f44ac1bacfdcc23b"
+}
+variable "keyname" {
+    default = "cal_key"
+}
+variable "windows_security_group" {
+    default = "sg-087c278e4c51a1523"
+}
+variable "linux_security_group" {
+    default = "sg-087c278e4c51a1523"
+}

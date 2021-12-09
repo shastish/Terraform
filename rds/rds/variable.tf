fa30349c43122bb71d86cba7d49e9bf8fd9c104c
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-09 12:00:23 +0530

    Create variable.tf

---
+++ rds/rds/variable.tf
@@ -0,0 +1,16 @@
+variable "vpcid" {
+    default = "vpc-0ba27102c0570d4dd"
+  
+}
+variable "rds_subnet1" {
+    default = "subnet-0d36e2815d492b8d5"
+  
+}
+variable "rds_subnet2" {
+    default = "subnet-024e3fc9323fe9df0"
+  
+}
+variable "db_instance" {
+    default = "db.t2.micro"
+  
+}

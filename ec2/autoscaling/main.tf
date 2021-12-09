f2853ca9c19901b702132a16e2633f7f97724d35
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-09 11:08:13 +0530

    Update main.tf

--- ec2/autoscaling/main.tf
+++ ec2/autoscaling/main.tf
@@ -10,4 +10,5 @@ module "auto_scaling" {
   vpc_id           = "vpc-0ba27102c0570d4dd"
   subnet1          = "subnet-0c7c1bc41cf0b7e93" //Prodcution-Public-AZ1a
   subnet2          = "subnet-0efa44c305c444ca4" //Prodcution-Public-AZ1b
+  target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:326717932327:targetgroup/apache-tg/b9cce9a5dcaefe68"
 }

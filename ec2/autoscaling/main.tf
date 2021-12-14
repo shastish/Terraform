4aa453ffb1a077658426cc53d3c8676958f254fc
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-14 10:22:53 +0530

    Update main.tf

--- ec2/autoscaling/main.tf
+++ ec2/autoscaling/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 ###############################################################
 module "auto_scaling" {
   source           = "./auto_scaling"
-  vpc_id           = "vpc-0ba27102c0570d4dd"
-  subnet1          = "subnet-0c7c1bc41cf0b7e93" //Prodcution-Public-AZ1a
-  subnet2          = "subnet-0efa44c305c444ca4" //Prodcution-Public-AZ1b
+  vpc_id           = "vpc-0ba27102c0570d4dd" //change
+  subnet1          = "subnet-0c7c1bc41cf0b7e93" //change-AZ1a
+  subnet2          = "subnet-0efa44c305c444ca4" //change-AZ1b
 }

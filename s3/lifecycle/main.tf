0e81bb02cd9dc680223b539f69129db2eec93da1
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-15 10:25:02 +0530

    Update main.tf

--- s3/lifecycle/main.tf
+++ s3/lifecycle/main.tf
@@ -38,25 +38,3 @@ resource "aws_s3_bucket" "bucket" {
     }
   }
 }
-resource "aws_s3_bucket" "versioning_bucket" {
-  bucket = "amazonmumbaitestbucketumesh" //S3 bucket will be created
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  lifecycle_rule {
-    prefix  = "Jenkins/"
-    enabled = true
-    noncurrent_version_transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-    noncurrent_version_transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-    noncurrent_version_expiration {
-      days = 90
-    }
-  }
-}

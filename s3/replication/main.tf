1bf93d6486c5157db2e6a462751701c352852b50
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-15 10:56:54 +0530

    Create main.tf

---
+++ s3/replication/main.tf
@@ -0,0 +1,111 @@
+#Declaring AWS Provider and credentials
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AKIAUYEPIAMT74ECYOKD"
+  secret_key = "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"
+}
+#######################################################
+
+resource "aws_iam_role" "replication" {
+  name = "s3-replication-rule"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "replication" {
+  name = "s3-replication-policy"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.source.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersionForReplication",
+        "s3:GetObjectVersionAcl",
+         "s3:GetObjectVersionTagging"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.source.arn}/*"
+      ]
+    },
+    {
+      "Action": [
+        "s3:ReplicateObject",
+        "s3:ReplicateDelete",
+        "s3:ReplicateTags"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.destination.arn}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "replication" {
+  role       = aws_iam_role.replication.name
+  policy_arn = aws_iam_policy.replication.arn
+}
+resource "aws_s3_bucket" "destination" {
+  #region = "ap-southeast-1"
+  bucket = "umesh-destination"
+
+  versioning {
+    enabled = true
+  }
+}
+resource "aws_s3_bucket" "source" {
+  #region = "ap-southeast-1"
+  bucket   = "umesh-source"
+  acl      = "private"
+  versioning {
+    enabled = true
+  }
+  replication_configuration {
+    role = aws_iam_role.replication.arn
+    rules {
+      id     = "Demo"
+      status = "Enabled"
+      filter {
+        tags = {}
+      }
+      destination {
+        bucket        = aws_s3_bucket.destination.arn
+        storage_class = "GLACIER"
+        replication_time {
+          status  = "Enabled"
+          minutes = 15
+        }
+        metrics {
+          status  = "Enabled"
+          minutes = 15
+        }
+      }
+    }
+  }
+}

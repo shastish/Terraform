ceb70dba7bca25c26f98ca34205a542c7dd63ccf
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-16 13:34:40 +0530

    Update main.tf

--- s3/staticwebsite/main.tf
+++ s3/staticwebsite/main.tf
@@ -1,21 +1,25 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "s3-website-test.hashicorp.com"
+#Declaring AWS Provider and credentials
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAWQ6CFPKVYENJ26DL"
+  secret_key = "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"
+}
+#######################################################
+resource "aws_s3_bucket" "bucket" {
+  bucket = "umeshumeshumeshumesh"
   acl    = "public-read"
-  policy = file("policy.json")
-
+  force_destroy = "true"
   website {
     index_document = "index.html"
-    error_document = "error.html"
-
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-EOF
   }
+}
+##########################################################
+resource "aws_s3_bucket_object" "object" {
+
+  bucket = aws_s3_bucket.bucket.id
+  key = "Welcome"
+  provisioner "local-exec" {
+     command = "aws s3 cp F:/welcome.html s3://${aws_s3_bucket.bucket.id}"
+  }
+  #syntax aws s3 cp sourcepath destinatiopath<s3://bucket name>
 }

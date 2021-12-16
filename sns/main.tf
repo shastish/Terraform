b2f8985fa71f8ee96df2c3925b905354f69bb5e5
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-16 11:40:29 +0530

    Create main.tf

---
+++ sns/main.tf
@@ -0,0 +1,35 @@
+#Declaring AWS Provider and credentials
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIAWQ6CFPKVYENJ26DL"
+  secret_key = "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"
+}
+#######################################################
+resource "aws_sns_topic" "alarm" {
+  name = "alarms-topic"
+
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+}
+resource "aws_sns_topic_subscription" "subscription" {
+  topic_arn = aws_sns_topic.alarm.arn
+  protocol  = "email"
+  endpoint  = "umeshbalaji987@gmail.com"
+}

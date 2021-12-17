a68286d302df050200e20fe942fa1a3e1674e900
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-12-17 10:02:54 +0530

    Update main.tf

--- s3/staticwebsite/main.tf
+++ s3/staticwebsite/main.tf
@@ -5,21 +5,23 @@ provider "aws" {
   secret_key = "+3j4V1m06B5GrQX4iyRHZICRcio28sJfxJwjW8Pj"
 }
 #######################################################
+#to create a bucket and enable static website
 resource "aws_s3_bucket" "bucket" {
   bucket = "umeshumeshumeshumesh"
-  acl    = "public-read"
-  force_destroy = "true"
-  website {
+  acl    = "public-read"                        #this gives public readonly premission 
+  force_destroy = "true"                        #will delete the bucket evn if it has object in it
+  website {                                     #this block define/enable the static website 
     index_document = "index.html"
   }
 }
 ##########################################################
+//this block is useed to upload a file in s3 bukcet
 resource "aws_s3_bucket_object" "object" {
 
   bucket = aws_s3_bucket.bucket.id
   key = "Welcome"
   provisioner "local-exec" {
-     command = "aws s3 cp F:/welcome.html s3://${aws_s3_bucket.bucket.id}" #To run this command aws cli should be install on the source machine
+     command = "aws s3 cp F:/welcome.html s3://${aws_s3_bucket.bucket.id}" //To run this command aws cli should be install on the source machine
   }
-  #syntax aws s3 cp sourcepath destinatiopath<s3://bucket name>
+  //syntax aws s3 cp sourcepath destinatiopath<s3://bucket name>
 }
